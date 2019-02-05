afce5bec3429cf3a61039708d227238c3e3a46dd
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2019-02-04 20:22:52 -0800

    Add clang-format configuration and run on code

--- main.cpp
+++ main.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 
 int main() {
-  int nx = 200;
-  int ny = 100;
+    int nx = 200;
+    int ny = 100;
 
-  // PPM header information.
-  std::cout << "P3" << std::endl;
-  std::cout << nx << " " << ny << std::endl;
-  std::cout << "255" << std::endl;;
+    // PPM header information.
+    std::cout << "P3" << std::endl;
+    std::cout << nx << " " << ny << std::endl;
+    std::cout << "255" << std::endl;
+    ;
 
-  // PPM image pixel values.
-  for (int j = ny - 1; j >= 0; j--) {
-    for (int i = 0; i < nx; i++) {
-      float r = float(i) / float(nx);
-      float g = float(j) / float(ny);
-      float b = 0.2;
-      int ir = int(255.99 * r);
-      int ig = int(255.99 * g);
-      int ib = int(255.99 * b);
-      std::cout << ir << " " << ig << " " << ib << std::endl;;
+    // PPM image pixel values.
+    for (int j = ny - 1; j >= 0; j--) {
+        for (int i = 0; i < nx; i++) {
+            float r = float(i) / float(nx);
+            float g = float(j) / float(ny);
+            float b = 0.2;
+            int ir = int(255.99 * r);
+            int ig = int(255.99 * g);
+            int ib = int(255.99 * b);
+            std::cout << ir << " " << ig << " " << ib << std::endl;
+            ;
+        }
     }
-  }
 }
