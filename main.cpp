12215a7c65479fb4682781b3c19c5cb36db865e7
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2019-02-04 20:12:39 -0800

    Document header and data creation code

--- main.cpp
+++ main.cpp
@@ -3,7 +3,13 @@
 int main() {
   int nx = 200;
   int ny = 100;
-  std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+
+  // PPM header information.
+  std::cout << "P3" << std::endl;
+  std::cout << nx << " " << ny << std::endl;
+  std::cout << "255" << std::endl;;
+
+  // PPM image pixel values.
   for (int j = ny - 1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
       float r = float(i) / float(nx);
@@ -12,7 +18,7 @@ int main() {
       int ir = int(255.99 * r);
       int ig = int(255.99 * g);
       int ib = int(255.99 * b);
-      std::cout << ir << " " << ig << " " << ib << "\n";
+      std::cout << ir << " " << ig << " " << ib << std::endl;;
     }
   }
 }
