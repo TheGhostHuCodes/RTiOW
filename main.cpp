e02279bba0a804ad8bb1f507550019a46048b70b
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2019-02-06 22:54:47 -0800

    Add diffuse material scattering

--- main.cpp
+++ main.cpp
@@ -7,12 +7,23 @@
 std::default_random_engine generator;
 std::uniform_real_distribution<double> distribution(0.0, 1.0);
 
+vec3 random_in_unit_sphere() {
+    vec3 p;
+    do {
+        p = 2.0
+                * vec3(distribution(generator), distribution(generator),
+                       distribution(generator))
+            - vec3(1, 1, 1);
+    } while (p.squared_length() >= 1.0);
+    return p;
+}
+
 vec3 color(const ray& r, hitable* world) {
     hit_record rec;
     if (world->hit(r, 0.0, MAXFLOAT, rec)) {
-        return 0.5
-               * vec3(rec.normal.x() + 1, rec.normal.y() + 1,
-                      rec.normal.z() + 1);
+        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * color(ray(rec.p, target - rec.p), world);
+
     } else {
         vec3 unit_direction = unit_vector(r.direction());
         float t = 0.5 * (unit_direction.y() + 1.0);
