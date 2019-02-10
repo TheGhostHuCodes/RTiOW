c5d50a585e88bde41e5f6c070ad69ef7bf0c5232
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2019-02-09 19:29:29 -0800

    Add large scene with multiple objects

--- main.cpp
+++ main.cpp
@@ -26,9 +26,52 @@ vec3 color(const ray& r, hitable* world, int depth) {
     }
 }
 
+hitable* random_scene() {
+    int n = 500;
+    hitable** list = new hitable*[n + 1];
+    list[0] = new sphere(vec3(0, -1000, 0), 1000,
+                         new lambertian(vec3(0.5, 0.5, 0.5)));
+    int i = 1;
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            float choose_mat = distribution(generator);
+            vec3 center(a + 0.9 * distribution(generator), 0.2,
+                        b + 0.9 * distribution(generator));
+            if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
+                if (choose_mat < 0.8) { // diffuse
+                    list[i++] = new sphere(
+                        center, 0.2,
+                        new lambertian(vec3(
+                            distribution(generator) * distribution(generator),
+                            distribution(generator) * distribution(generator),
+                            distribution(generator)
+                                * distribution(generator))));
+                } else if (choose_mat < 0.95) { // metal
+                    list[i++] = new sphere(
+                        center, 0.2,
+                        new metal(vec3(0.5 * (1 + distribution(generator)),
+                                       0.5 * (1 + distribution(generator)),
+                                       0.5 * (1 + distribution(generator))),
+                                  0.5 * distribution(generator)));
+                } else { // glass
+                    list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+                }
+            }
+        }
+    }
+
+    list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+    list[i++]
+        = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+    list[i++]
+        = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+    return new hitable_list(list, i);
+}
+
 int main() {
-    int nx = 200;
-    int ny = 100;
+    int nx = 2560;
+    int ny = 1600;
     int ns = 100;
 
     // PPM header information.
@@ -47,11 +90,12 @@ int main() {
     list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
     list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
     hitable* world = new hitable_list(list, 5);
+    world = random_scene();
 
-    vec3 look_from(3, 3, 2);
-    vec3 look_at(0, 0, -1);
+    vec3 look_from(13, 2, 3);
+    vec3 look_at(0, 0, 0);
     float dist_to_focus = (look_from - look_at).length();
-    float aperture = 2;
+    float aperture = 0.1;
     camera cam(look_from, look_at, vec3(0, 1, 0), 20, float(nx) / float(ny),
                aperture, dist_to_focus);
     for (int j = ny - 1; j >= 0; j--) {
